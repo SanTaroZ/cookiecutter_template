c4e4b5b1be0ef8816bf0c8ff3bdcf85e2703d6a1
Edwin Cardenas <cedwin72@gmail.com>
2021-12-23 19:01:10 -0400

    Se modificaron algunos archivos

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -11,4 +11,4 @@ subprocess.call(['git', 'init'])
 subprocess.call(['git', 'add', '*'])
 subprocess.call(['git', 'commit', '-m', 'Initial commit'])
 
-print(f"{MESSAGE_COLOR}git init, git add * & git commit -m Initial has been executed{RESET_ALL}")
+print(f"{MESSAGE_COLOR} git init, git add * & git commit -m Initial has been executed{RESET_ALL}")
