e9089ce1fd578e1ef6cc192e2b781c88be84ef5d
Edwin Cardenas <cedwin72@gmail.com>
2022-03-13 18:52:29 -0400

    post_gen

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,7 +4,7 @@ import subprocess
 MESSAGE_COLOR = "\x1b[34m"
 RESET_ALL = "\x1b[0m"
 
-print(f"{MESSAGE_COLOR}Installing libraries")
+print(f"{MESSAGE_COLOR}Installing libraries...")
 #print(f"Initializing a git repository...{RESET_ALL}")
 
 #subprocess.call(['git', 'init'])
@@ -12,7 +12,7 @@ print(f"{MESSAGE_COLOR}Installing libraries")
 #subprocess.call(['git', 'commit', '-m', 'Initial commit'])
 
 #print(f"{MESSAGE_COLOR} git init, git add * & git commit -m Initial has been executed{RESET_ALL}")
-subprocess.call(['dir'])
+#subprocess.call(['dir'])
 #subprocess.call(['pip', 'install','-r','requirements.txt'])
 
 print(f"{MESSAGE_COLOR}Done!!")
