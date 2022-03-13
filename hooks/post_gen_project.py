481b69e588189f6843945b54f0f06d85d9658b63
Edwin Cardenas <cedwin72@gmail.com>
2022-03-13 18:51:13 -0400

    post_gen

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -12,7 +12,7 @@ print(f"{MESSAGE_COLOR}Installing libraries")
 #subprocess.call(['git', 'commit', '-m', 'Initial commit'])
 
 #print(f"{MESSAGE_COLOR} git init, git add * & git commit -m Initial has been executed{RESET_ALL}")
-subprocess.call(['ls'])
+subprocess.call(['dir'])
 #subprocess.call(['pip', 'install','-r','requirements.txt'])
 
 print(f"{MESSAGE_COLOR}Done!!")
